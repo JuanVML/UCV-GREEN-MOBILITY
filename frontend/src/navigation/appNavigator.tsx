a729e58c9852bfa0cf4f09b3a2a9b37e04a65949
Juan VML <jmunozlo01@ucvvirtual.edu.pe>
2025-09-22 09:23:05 -0500

    splash terminado

--- frontend/src/navigation/appNavigator.tsx
+++ frontend/src/navigation/appNavigator.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen from "../screens/SplashScreen";
 import Login from "../screens/Login";
 import Registro from "../screens/Registro";
 import { AuthContext } from "../context/AuthContext";
@@ -12,13 +13,14 @@ const AppNavigator = () => {
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {user ? (
-        <Stack.Screen name="Main" component={TabNavigator} />
-      ) : (
+      {!user ? (
         <>
+          <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Registro" component={Registro} />
         </>
+      ) : (
+        <Stack.Screen name="Main" component={TabNavigator} />
       )}
     </Stack.Navigator>
   );
---
+++ frontend/src/screens/SplashScreen.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+
+const SplashScreen = ({ navigation }: any) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace("Login");
+    }, 3000); // 2 segundos
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Image
+          source={require("../../assets/images/logo2.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>UCV Green Mobility</Text>
+        <Text style={styles.subtitle}>
+          Comparte el camino hacia{"\n"}un campus más sostenible
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0B6A75",
+    justifyContent: "center", // centra vertical
+    alignItems: "center", // centra horizontal
+    paddingHorizontal: 50,
+  },
+  content: {
+    alignItems: "center", // centra logo y textos
+  },
+logo: {
+  width: 500,
+  height: 470,
+  marginBottom: -170,
+  alignSelf: "flex-start", // lo manda a la izquierda
+  marginLeft: 30, 
+  marginRight : 150,         // ajusta cuanto se mueve a la izquierda
+},
+
+  title: {
+    color: "#fff",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+    letterSpacing: 1,
+    textAlign: "center",
+  },
+  subtitle: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: "center",
+    lineHeight: 26,
+  },
+});
+
+export default SplashScreen;
